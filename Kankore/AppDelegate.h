a7a575ea8736211b50451b42f3e9e87d36659991
kamakura-masaya <amethyst.aqua@gmail.com>
2013-09-01 15:17:57 +0900

    Initial Commit

---
+++ Kankore/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Kankore
+//
+//  Created by 鎌倉 正也 on 2013/09/01.
+//  Copyright (c) 2013年 Masaya Kamakua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Kankore/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Kankore
+//
+//  Created by 鎌倉 正也 on 2013/09/01.
+//  Copyright (c) 2013年 Masaya Kamakua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
